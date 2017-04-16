d2cb09bfb2325b694d5419b795c574a7be3c82ed
Y.Suzuki <y.suzuki089@gmail.com>
2017-04-16 23:45:15 +0900

    改めてプロジェクトを新規追加

---
+++ ProjectVR/Assets/Script/camera/scr_VRCameraRoot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class scr_VRCameraRoot : MonoBehaviour {
+
+    public GameObject king = null;
+    public float cameraHeight = 2.0f;
+
+    private Quaternion cameraRotation;
+    public Quaternion CameraRotation
+    {
+        get { return cameraRotation; }
+    }
+
+    private Vector3 forward;
+    public Vector3 Forward
+    {
+        get { return forward; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        if( !king )
+        {
+    		king = GameObject.Find("DummyKing");
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if( VRSettings.enabled )
+        {
+            Quaternion trackingRot = InputTracking.GetLocalRotation(VRNode.CenterEye);
+            cameraRotation = trackingRot;
+
+            Vector3 worldPos = transform.position + InputTracking.GetLocalPosition(VRNode.CenterEye);
+            Vector3 transformedPos = trackingRot * worldPos;
+            
+            forward = Vector3.Normalize(transformedPos);
+        }
+	}
+
+    void LateUpdate()
+    {
+        MoveExe();
+
+        Vector3 trackingPos = InputTracking.GetLocalPosition(VRNode.CenterEye);
+        {
+            string strInfo = "Tracking Pos:";
+            strInfo += trackingPos.ToString() + "\n";
+
+            Vector3 camPos = Camera.main.transform.position;
+            strInfo += "Camera Pos:";
+            strInfo += camPos.ToString() + Camera.main.name + "\n";
+
+            scr_GUIText.instance.AddText(strInfo);
+        }
+    }
+
+    /**
+     *  @brief      アタッチ済みの王様オブジェクトに付随して動く
+     */
+    void MoveExe()
+    {
+        if( !king ) return;
+
+        Vector3 kingPos = king.transform.position;
+        Vector3 objPos = transform.position;
+        objPos = kingPos;
+        objPos.y += cameraHeight;
+        transform.position = objPos;        
+
+    }
+
+}
---
+++ ProjectVR/Assets/Script/scr_GUIText.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_GUIText : MonoBehaviour {
+
+    private GameObject textObj = null;
+    private bool bDraw = false;
+
+    // シングルトン
+    private static scr_GUIText _instance;
+    public static scr_GUIText instance
+    {
+        get
+        {
+            if( _instance == null )
+            {
+                _instance = FindObjectOfType<scr_GUIText>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+            return _instance;
+        }
+    }
+
+    public void Awake()
+    {
+        if( _instance == null )
+        {
+            _instance = this;
+            DontDestroyOnLoad(_instance);
+        }
+        else if( this != _instance )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		textObj = GameObject.Find("Text");
+        bDraw = false;
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( !textObj ) return;
+
+        if( Input.GetButtonDown("L1") )
+        {
+            bDraw = !bDraw;
+        }
+
+        textObj.GetComponent<GUIText>().text = "";
+	}
+
+    void LateUpdate()
+    {
+    }
+    
+    public void SetText(string str)
+    {
+        if( !textObj ) return;
+        if( !bDraw ) return;
+
+        textObj.GetComponent<GUIText>().text = str;
+    }
+
+    public void AddText(string str)
+    {
+        if( !textObj ) return;
+        if( !bDraw ) return;
+
+        textObj.GetComponent<GUIText>().text += str;
+    }
+
+    public void SetDrawFlag(bool flg)
+    {
+        bDraw = flg;
+    }
+}
