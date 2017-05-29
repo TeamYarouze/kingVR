343c5ed7f630676dd7eab622f786dc412fefa732
Y.Suzuki <y.suzuki089@gmail.com>
2017-05-30 01:34:09 +0900

    諸々リソース追加忘れ

---
+++ ProjectVR/Assets/Script/Item/ItemWeight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemWeight : ItemBase {
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( !IsAttachedObject() )
+        {
+            return;
+        }
+
+        UpdateReloadTime();
+		
+        OnFire();
+	}
+
+    public void FixedUpdate()
+    {
+        if( !IsAttachedObject() )
+        {
+            return;
+        }
+    }
+
+    //---------------------------------------------------------------
+    /*
+        @brief      発射処理
+    */
+    //---------------------------------------------------------------
+    new public bool OnFire()
+    {
+        return false;
+    }
+
+}
---
+++ ProjectVR/ResorceCopy.bat
@@ -0,0 +1,8 @@
+@echo on
+
+copy /y Assets\Prefab\Item\Rocket.prefab Assets\Resources\Prefab
+copy /y Assets\Prefab\Item\Weight.prefab Assets\Resources\Prefab
+
+
+
+
