f1bc668cc51e3250c53a4631060fbc502c8a4b43
稲田　周三 <moyasin2006@gmail.com>
2017-06-30 01:42:23 +0900

    集中線を追加。

---
+++ ProjectVR/Assets/Script/rollingwave_l.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rollingwave_l : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        iTween.RotateBy(this.gameObject, iTween.Hash("z", 1, "easeType", "linear", "loopType", "loop", "time", 0.1f));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
